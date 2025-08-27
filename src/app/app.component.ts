faa526ec77196fb5e2980fb995fea3f9c21ef230
MatheusHG <matheus.oliveira@ccc.ufcg.edu.br>
2025-08-27 18:40:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend-promotion';
+  title = 'front-promotion';
+
+  constructor() {
+    register();
+  }
 }
